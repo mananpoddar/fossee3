3ce762393ccf4d778837ee09fbe73e43cb5ac18b
mananpoddar <mananpoddarm@gmail.com>
2019-03-15 15:10:23 +0530

    rendering slides and validation left

--- mysite/fossee3/static/fossee3/topnav.js
+++ mysite/fossee3/static/fossee3/topnav.js
@@ -1,29 +1,25 @@
 function callaboutme(){
     document.getElementById("demo").innerHTML=" About this project <br><br>"+
-   " This web application is built on the top of django , it allows you to conveniently share your codes with anyone with just one link !"+
-      " <div style=text-indent: 1em>It also allows you to manage your shared codes so that you can refer them whenever you need it.I"+
-      "would really like to know how you feel about the application , to give feedback you can contact me through contact section on the navbar ! </div>"
-    
+   " This web application is built on the top of django , Developed "+
+   "as an assignment for fossee 2019. <br> It allows you to upload "+
+   "multiple images and refer as and when required"
+   
 }
 
-function callwhywebclub(){
+function callwhyfossee(){
+console.log("sdef")
 document.getElementById("demo").innerHTML=
-"  I want to be the part of web club"+
-" because i believe that i can learn a lot from the seniors there"+
-" also i think getting into web club after all the efforts i put into this application will give me an immense satisfaction and"+
-" a feeling of pride. so basically this application can be the gateway for me to the club,also some of my personal favourite seniors are in the club "+
-", this provides me an golden oppurtunity to interact with them even more  that's the reason i have put so much effort into it. "+
-"Iam relatively week in DSA because i learnt web dev in my summer but my willingness to learn will never disappoint you "+
-"and i promise if i get into the club i will work so hard that you will get the results from my side for sure ."
-                       }
-function callhowitstarted(){
-    document.getElementById("demo").innerHTML="This is the assignment for the recruitments of web club"+
-    ". I always thought that this will be an application which can really serve a lot of people"+
-    " okay one question for you guys , what if you want to share a code with your friend to debug , can you think of a platform which is convenient "+
-    "to you . well if your answer is no you have come to the right place."
+"I want to be the part of fossee team at IITB because I really like working with teams developing"+
+" real life applications.At IITB labs I can make my summer productive, Some of my seniors told me"+
+" about their experience of being a part of fossee and it really motivated me to complete the tasks"+
+", that too on time. Fossee is perfect for my case because I already have gained descent experience"+
+" in the field of web dev and my github heat map will speak the same. I am confident enough to work"+
+" on the projects. I want to use every single minute of my summers and I am pretty sure"+
+" that if I get selected, fossee will help me in that."
 }
+
 
 function callcontact(){
       document.getElementById("demo").innerHTML="<ul><li>&#35; 101,Block 3,NITK,  SURATHKAL</li><li>INDIA</li><li>+91&nbsp;9113536695</li><li><a href=mailto:mananpoddarm@gmail.com style=text-decoration:none>&#9993;"+
-     " mananpoddarm@gmail.com</a></li></ul>"
+     " mananpoddarm@gmail.com</a></li> <li><a href = https://mananpoddar.github.io> Personal Website</a></li>  </ul>"
 }
