f4c8e0552807b3649e567531b3d6b5ab12b9df8b
mananpoddar <mananpoddarm@gmail.com>
2019-03-17 16:36:15 +0530

    ready for submission

--- mysite/fossee3/static/fossee3/ajax.js
+++ mysite/fossee3/static/fossee3/ajax.js
@@ -1,47 +1,77 @@
+var inputDocument = document.getElementById("inputDocument");
+var fileList = [];
 
-        var inputDocument = document.getElementById('inputDocument');
-        var fileList = [];
-        
-        inputDocument.addEventListener('change',function(e){
-            for(var i =0;i<inputDocument.files.length;i++){
-                fileList.push(inputDocument.files[i]);
-                console.log('is it adsadfding Document');
-            }
-        })
-      
-        var fileCatcher = document.getElementById('file-catcher')
-        fileCatcher.addEventListener('submit',function(e){
-            e.preventDefault();
-            fileList.forEach(function(file){
-                
-                sendFile(file);
-                console.log("sendasdfing the files");
-            });
-           //after sending all the files making a get request to viewDocument 
-           window.location.href="/fossee3/viewDocument";
+//the below logic is keeping track of whenever user enters that input button for inputting the file
+//we keep saving those files in fileList array(if the files are valid)
+inputDocument.addEventListener("change", function(e) {
+  for (var i = 0; i < inputDocument.files.length; i++) {
+    console.log(inputDocument.files[i].name);
 
-        });
-        
-        sendFile = function(file){
-           
-            var title       =   document.getElementById('title').value
-            var description =   document.getElementById('description').value
-        
-            var formData = new FormData();
-            formData.append("Document",file)
-            formData.append("title",title)
-            formData.append("description",description)
+    //check if the extension of file is valid
+    name = String(inputDocument.files[i].name);
+    var l = name.split(".", 2);
+    var extn = String(l[1]);
+    //if valid sendFile otherwise not
+    if (
+      extn.localeCompare("ppt") == 0 ||
+      extn.localeCompare("pptx") == 0 ||
+      extn.localeCompare("pps") == 0
+    ) {
+      var node = document.createElement("LI"); // Create a <li> node
+      var textnode = document.createTextNode(name); // Create a text node
+      node.appendChild(textnode); // Append the text to <li>
+      document.getElementById("displayFile").appendChild(node);
+      fileList.push(inputDocument.files[i]);
+      console.log("sending Document");
+    } else {
+      console.log("it's not a valid extension");
+      var node = document.createElement("LI"); // Create a <li> node
+      var textnode = document.createTextNode(
+        "only ppt,pptx and pps extensions are allowed, please enter file with valid extensions"
+      ); // Create a text node
+      node.appendChild(textnode); // Append the text to <li>
+      document.getElementById("displayFile").appendChild(node);
+    }
+  }
+});
 
+//the below logic is activated when the user hits submit button, one by one we make ajax
+//request to the server for sending the files in fileList array
+var fileCatcher = document.getElementById("file-catcher");
+fileCatcher.addEventListener("submit", function(e) {
+  e.preventDefault();
+  console.log(fileList.length);
+  fileList.forEach(function(file) {
+    sendFile(file);
+  });
+  //after sending all the files making a get request to viewDocument
+  setTimeout(getRequest, 1000);
+});
 
-          
-            jQuery.ajax({
-                url: "",
-                type: "POST",
-                data:formData,
-                processData: false,
-                contentType: false,
-                success:function(){
-                    console.log("done successfully");
-                }
-            });
-        };
+//sending get request after 1000ms to make sure that the server has saved all the files 
+//which we sent through ajax
+getRequest = function() {
+  window.location.href = "/fossee3/viewDocument";
+};
+
+//ajax logic for sending the files to the server
+sendFile = function(file) {
+  var title = document.getElementById("title").value;
+  var description = document.getElementById("description").value;
+
+  var formData = new FormData();
+  formData.append("Document", file);
+  formData.append("title", title);
+  formData.append("description", description);
+
+  jQuery.ajax({
+    url: "",
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function() {
+      console.log("done successfully");
+    }
+  });
+};
--- mysite/fossee3/views.py
+++ mysite/fossee3/views.py
@@ -3,36 +3,85 @@ from django.views.decorators.csrf import csrf_exempt
 from fossee3.models import Caption,Documents
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+import subprocess
+import shutil
+import ntpath
+import os
+ntpath.basename("/media/document/")
+wdFormatPDF = 17
 
-ini =0
 
-# Create your views here.
-
+# argument: request 
+# what it does:renders the front page 
+# returns: returns the front page
 def index(request):
     return render(request,"fossee3/index.html")
 
 
+# argument: request 
+# what it does:get's the file from user one by one through ajax requests,
+#              saves it in the database
+# returns: returns the page where user can upload the slides
 @csrf_exempt
 def uploadDocument(request):
+    
     if request.method=="POST":
-        ini =0
-        if ini == 0:
-                caption = Caption(
-                title = request.POST.get('title'),
-                description = request.POST.get('description')
-                )
-                caption.save()
-                ini =1
+        fileUrl = request.FILES.get('Document') 
+        
+    
+        caption = Caption(
+        title = request.POST.get('title'),
+        description = request.POST.get('description')
+        )
+        caption.save()
+        # conversiontopdf(fileUrl)
+
         Document = Documents(
             document = request.FILES.get('Document')
 
         )
         Document.save()
-        print(Document.is_valid())
+        fileUrl = Document.document.url
+        conversiontopdf(fileUrl)
+  
     return render(request,"fossee3/uploadDocument.html")
 
+
+# argument: request 
+# what it does:gets all the documents from the database
+# returns: returns the page rendering all these documents with viewer link to it
 def viewDocument(request):
     print("viewDocument")
     caption = Caption.objects.all()[0]
     Document = Documents.objects.all()
+    
+    #saving pdf path to every object in document to render the pdf's
+    for obj in Document:
+        head, tail = ntpath.split('./'+str(obj.document) )
+        tail = tail.split('.')[0] + '.pdf'
+        obj.document = 'document/'+tail
+        obj.save()
     return render(request,"fossee3/viewDocuments.html",{"caption":caption,"Document":Document})
+
+
+# argument:path of the file
+# what it does: since I was not able to find viewer for extensions other than pdf, 
+#               I converted the files to pdf first and saved it in the database along 
+#               with the original files, so that I can render all the files through the viewers
+#               so,basically this function converts all the files to pdf and keep a copy of that as well in database     
+# returns: doesn't return anything
+def conversiontopdf(fileUrl):
+    fileUrl = '.'+fileUrl
+    head, tail = ntpath.split(fileUrl)
+    tail = tail.split('.')[0] + '.pdf'
+    src = tail
+    dest = './media/document'
+    output = subprocess.check_output(['libreoffice', '--convert-to', 'pdf' ,fileUrl])
+    try:
+        shutil.move(src,dest)
+    except:
+        os.remove(src)
+    
+
+   
+
